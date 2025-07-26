b6715a8883effedc0c35cf4dc07dc9c3b4801831
Stan <stanfalkenrich@gmail.com>
2025-07-26 21:57:07 +0200

    add tests, refactor code to match specs

--- palindromeChecker/src/services/PalindromeCheckerService.cs
+++ palindromeChecker/src/services/PalindromeCheckerService.cs
@@ -1,12 +1,25 @@
 using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace palindromeChecker.src.services
 {
-    public class PalindromeCheckerService
+    public partial class PalindromeCheckerService
     {
-        public Boolean IsValidPalindrome(string input)
+        [GeneratedRegex(@"[^a-zA-Z0-9]")]
+        private static partial Regex IsNotSpecialCharacterOrWhiteSpace();
+        public static Boolean IsValidPalindrome(string? input)
         {
-            return true;
+            if (input != null)
+            {
+                var trimmedToLower = IsNotSpecialCharacterOrWhiteSpace().Replace(input, "").ToLower();
+                char[] charArary = trimmedToLower.ToCharArray();
+                Array.Reverse(charArary);
+                var reversed = new string(charArary);
+                return String.Equals(reversed, trimmedToLower);
+            }
+            return false;
         }
+    
     }
 }
--- palindromeChecker/test/PalindromeCheckerServiceTest.cs
+++ palindromeChecker/test/PalindromeCheckerServiceTest.cs
@@ -1,20 +1,36 @@
 ﻿namespace palindromeChecker.test;
 
+using System.Runtime.CompilerServices;
 using NUnit.Framework;
 using palindromeChecker.src.services;
 
+[TestFixture]
 public class PalindromeCheckerServiceTest
 {
-      private PalindromeCheckerService _service;
+    private PalindromeCheckerService _service = new PalindromeCheckerService();
     [SetUp]
     public static void setUp()
     {
-
     }
     [Test]
-    public void Test()
+    public void TestThatNoPalindromeIsInvalid()
     {
-        Assert.Pass();
+        var result = PalindromeCheckerService.IsValidPalindrome("Hi");
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void TestThatPalindromeIsValid()
+    {
+        var result = PalindromeCheckerService.IsValidPalindrome("A man, a plan, a canal: Panama");
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void TestThatNullIsNotValidPalindrome()
+    {
+        var result = PalindromeCheckerService.IsValidPalindrome(null);
+        Assert.That(result, Is.False);
     }
 
 }
