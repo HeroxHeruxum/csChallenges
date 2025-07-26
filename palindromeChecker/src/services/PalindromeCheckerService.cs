5558836a798b714dc94a3445bd69215834361a7c
Stan <stanfalkenrich@gmail.com>
2025-07-26 19:36:15 +0200

    add test

---
+++ palindromeChecker/src/services/PalindromeCheckerService.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace palindromeChecker.src.services
+{
+    public class PalindromeCheckerService
+    {
+        public Boolean IsValidPalindrome(string input)
+        {
+            return true;
+        }
+    }
+}
---
+++ palindromeChecker/test/PalindromeCheckerServiceTest.cs
@@ -0,0 +1,20 @@
+﻿namespace palindromeChecker.test;
+
+using NUnit.Framework;
+using palindromeChecker.src.services;
+
+public class PalindromeCheckerServiceTest
+{
+      private PalindromeCheckerService _service;
+    [SetUp]
+    public static void setUp()
+    {
+
+    }
+    [Test]
+    public void Test()
+    {
+        Assert.Pass();
+    }
+
+}
